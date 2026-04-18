a6bbf688140bc1ef8c64c89b7199067ca2a240fb
Marc Galimany <galiaerialstraps@gmail.com>
2026-04-18 19:40:37 +0200

    Crear tareas en base de datos firebase, Leer tareas, modificar si estan realizadas y crear nuevas tareas

--- src/app/datos-firebase/datos-firebase.page.ts
+++ src/app/datos-firebase/datos-firebase.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem,IonLabel, IonList } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem,IonLabel, IonList,IonGrid,IonCol,IonRow,IonCard,
+        IonCardHeader,IonCardTitle ,IonCardSubtitle,IonCardContent,IonCheckbox,IonButton, IonInput} from '@ionic/angular/standalone';
 import { CRUDFirebase } from '../services/crud-firebase';
 
 @Component({
@@ -9,11 +10,22 @@ import { CRUDFirebase } from '../services/crud-firebase';
   templateUrl: './datos-firebase.page.html',
   styleUrls: ['./datos-firebase.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonItem,IonLabel,IonList]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonItem,IonLabel,IonList,IonGrid,IonCol,
+            IonRow,IonCard,IonCardHeader,IonCardTitle, IonCardSubtitle,IonCardContent,IonCheckbox,IonButton,IonInput ]
 })
 export class DatosFirebasePage implements OnInit {
 
   items: any[] = [];
+  Tarea ={
+    TituloTarea:'',
+    TipoTarea:'',
+    descripcion:'',
+    HorasEnRealizarTarea:'',
+    fechaCreacion:'',
+    TareaRealizada: false,
+  }
+
+
 
   constructor(private crudFirebase:CRUDFirebase ) { }
 
@@ -22,5 +34,16 @@ export class DatosFirebasePage implements OnInit {
       this.items = res;
     });
   }
+  ActualizarTareaRealizada(item:any,$event:any){
+    item.TareaRealizada = $event.detail.checked
+    this.crudFirebase.update(item.id,{TareaRealizada:item.TareaRealizada})
+  }
+  BorrarTarea(item:any){
+    console.log(item.id)
+    this.crudFirebase.delete(item.id)
+  }
 
+  NuevaTarea(){
+    this.crudFirebase.create(this.Tarea)
+  }
 }
